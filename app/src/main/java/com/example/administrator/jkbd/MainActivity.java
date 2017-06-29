687fc7bbf05038939f5f3c70b287042665f10338
WinnieWei0 <1344228500@qq.com>
2017-06-30 00:09:26 +0800

    OkHttpUtils使用

--- app/src/main/java/com/example/administrator/jkbd/MainActivity.java
+++ app/src/main/java/com/example/administrator/jkbd/MainActivity.java
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.administrator.jkbd.utils.OkHttpUtils;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -15,7 +17,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public void test(View view){
-startActivity(new Intent(MainActivity.this,ExamActivity.class));
+        OkHttpUtils<ExamInfo> utils=new OkHttpUtils<>(getApplicationContext());
+        String uri="http://101.251.196.90:8080/JztkServer/examInfo";
+        utils.url(uri).targetClass(ExamInfo.class).execute(new OkHttpUtils.OnCompleteListener<ExamInfo>(){
+            @Override
+            public void onSuccess(ExamInfo result) {
+log.e("main","result="+result);
+            }
+
+            @Override
+            public void onError(String error) {
+log.e("main","error="+error);
+            }
+        });
+        startActivity(new Intent(MainActivity.this,ExamActivity.class));
     }
     public void exit(View view){
 finish();
