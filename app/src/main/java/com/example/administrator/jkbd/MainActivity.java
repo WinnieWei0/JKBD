867a3561eb79de82a71e0695c1a6754a5b61af67
WinnieWei0 <18878476772a>
2017-06-29 16:50:52 +0800

    增加tools类型

--- app/src/main/java/com/example/administrator/jkbd/MainActivity.java
+++ app/src/main/java/com/example/administrator/jkbd/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.administrator.jkbd;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void test(View view){
+startActivity(new Intent(MainActivity.this,ExamActivity.class));
+    }
+    public void exit(View view){
+finish();
     }
 }
